8ae9542047f2017037ed76fde0cd5b33466aef61
Priscila Da Dalt <priscila.dalt@aluno.faculdadeimpacta.com.br>
2023-03-27 23:36:43 -0300

    Adicionado funcionalidades PWA

---
+++ src/SwitchButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SwitchButton = () => (
+    <div className="switch-container">
+      <div id="wonderpush-subscription-switch" data-on="YES" data-off="NO"></div>
+    </div>
+  )
+
+export default SwitchButton
--- src/index.js
+++ src/index.js
@@ -4,20 +4,20 @@ import './index.css';
 import Header from "./Header"
 import User from './User';
 import Content from './Content';
-import Menu from './Menu'
+import Menu from './Menu';
 import { WonderPush } from 'react-wonderpush';
+import SwitchButton from './SwitchButton';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* Wrap your app with our WonderPush component */}
     <WonderPush options = {{webKey: "a6a00ccf60ffee04cb201cead4304fc4db8fa8e0a2823a77466e8c8ebeed4e25"}}>
-      
+        <SwitchButton /> 
         <Header />
         <User/>
         <Menu />
         <Content />
-      
     </WonderPush>
   </React.StrictMode>
 );
