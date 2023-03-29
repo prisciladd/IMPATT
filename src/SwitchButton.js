bb5c8e3e2a1737293e1c171c0e5fb31066fe2254
Priscila Da Dalt <priscila.dalt@aluno.faculdadeimpacta.com.br>
2023-03-28 21:59:07 -0300

    Criar a funcionalidade "Add to Home screen"

--- src/SwitchButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SwitchButton = () => (
-    <div className="switch-container">
-      <div id="wonderpush-subscription-switch" data-on="YES" data-off="NO"></div>
-    </div>
-  )
-
-export default SwitchButton
