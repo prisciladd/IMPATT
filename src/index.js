671a8387e95c7f40bb29a2e7cdde930d4f568986
C1325300 Gabriel Dos Santos Motroni <gabriel.motroni@ibm.com>
2023-05-11 22:07:52 -0300

    correcao chamada telas

--- src/index.js
+++ src/index.js
@@ -9,34 +9,34 @@ import ModalBoletim from "./components/ModalBoletim";
 //import { A2HSProvider } from "react-a2hs";
 import App from "./App";
 
-let deferredPrompt;
-const addBtn = document.querySelector(".add-button");
-//addBtn.style.display = "none"; // ele quebra a aplicacao
+// let deferredPrompt;
+// const addBtn = document.querySelector(".add-button");
+// //addBtn.style.display = "none"; // ele quebra a aplicacao
 
-window.addEventListener("beforeinstallprompt", (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  addBtn.style.display = "block";
+// window.addEventListener("beforeinstallprompt", (e) => {
+//   // Prevent Chrome 67 and earlier from automatically showing the prompt
+//   e.preventDefault();
+//   // Stash the event so it can be triggered later.
+//   deferredPrompt = e;
+//   // Update UI to notify the user they can add to home screen
+//   addBtn.style.display = "block";
 
-  addBtn.addEventListener("click", (e) => {
-    // hide our user interface that shows our A2HS button
-    addBtn.style.display = "none";
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the A2HS prompt");
-      } else {
-        console.log("User dismissed the A2HS prompt");
-      }
-      deferredPrompt = null;
-    });
-  });
-});
+//   addBtn.addEventListener("click", (e) => {
+//     // hide our user interface that shows our A2HS button
+//     addBtn.style.display = "none";
+//     // Show the prompt
+//     deferredPrompt.prompt();
+//     // Wait for the user to respond to the prompt
+//     deferredPrompt.userChoice.then((choiceResult) => {
+//       if (choiceResult.outcome === "accepted") {
+//         console.log("User accepted the A2HS prompt");
+//       } else {
+//         console.log("User dismissed the A2HS prompt");
+//       }
+//       deferredPrompt = null;
+//     });
+//   });
+// });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -47,7 +47,8 @@ root.render(
           <Route exact path="/" component={Home} />
           <Route path="/NotasFaltas" component={NotasFaltas} />
           <Route path="/Boletim" component={Boletim} />
-          <Route path="/ModalBoletim" component={ModalBoletim} /> {/* Rota para o modal */}
+          <Route path="/ModalBoletim" component={ModalBoletim} />{" "}
+          {/* Rota para o modal */}
         </App>
       </Switch>
     </Router>
