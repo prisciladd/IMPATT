fc29281e8cb938264e39c63b927c937a798645bf
C1325300 Gabriel Dos Santos Motroni <gabriel.motroni@ibm.com>
2023-05-11 22:19:25 -0300

    media query pri

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,10 @@
 import ItemMenu from "./ItemMenu";
-import { useState } from 'react';
+import { useState } from "react";
 import styled from "styled-components";
 import { BoxTitle, BoxText, List, BoxSmall } from "../../pages/Home/HomeStyles";
 
-const StyledDiv= styled.div`
-
-/* @media (max-width:600px){
+const StyledDiv = styled.div`
+  /* @media (max-width:600px){
 
     #menu-vertical_all menu-expand{
         width:40%;
@@ -28,106 +27,113 @@ const StyledDiv= styled.div`
     }
 
     */
-`
+`;
 
 function Menu() {
-    const [showSubMenu, setShowSubMenu] = useState(false);
-    const [setShowNotasFaltas] = useState(false); // Novo estado para controlar a exibição da tela de Notas e Faltas
-    
-    function handleMouseOver() {
-        setShowSubMenu(true);
-    }
+  const [showSubMenu, setShowSubMenu] = useState(false);
+  const [setShowNotasFaltas] = useState(false); // Novo estado para controlar a exibição da tela de Notas e Faltas
 
-    function handleMouseOut() {
-        setShowSubMenu(false);
-    }
+  function handleMouseOver() {
+    setShowSubMenu(true);
+  }
 
-    function handleNotasFaltasClick() {
-        setShowNotasFaltas(true); // Define o estado para mostrar a tela de Notas e Faltas
-    }
+  function handleMouseOut() {
+    setShowSubMenu(false);
+  }
 
-    // Renderiza a tela de Notas e Faltas se o estado showNotasEFaltas for verdadeiro
-    // if (showNotasFaltas) {
-    //     return <NotasFaltas />;
-    // }
-    // Renderiza o componente Menu
-    return (
-        <StyledDiv>
-                <BoxSmall>
-                    <div className="menu-vertical_all menu-expand"
-                        id="menu-vertical_secretaria_online"
-                        onMouseOver={handleMouseOver}
-                        onMouseOut={handleMouseOut}>
-                        <header className="App-header">
-                            <BoxTitle>
-                                <h3>ACESSO RÁPIDO</h3>
-                            </BoxTitle>
-                        </header>
-                        <BoxText>
-                            <ItemMenu name="Carteirinha de Estudante" ></ItemMenu>
-                            <ItemMenu name="Secretaria Online" ></ItemMenu>
-                            <ItemMenu name="Financeiro Online" ></ItemMenu>
-                            <ItemMenu name="Biblioteca" ></ItemMenu>
-                            <ItemMenu name="PAI" ></ItemMenu>
-                            <ItemMenu name="CPA" ></ItemMenu>
-                            <ItemMenu name="Atividade Complementares" ></ItemMenu>
-                            <ItemMenu name="Ouvidoria" ></ItemMenu>
-                            <ItemMenu name="Requerimentos
-                            (Secretaria/Financeiro)" ></ItemMenu>
-                            <ItemMenu name="Contato dos Professores" ></ItemMenu>
-                            <ItemMenu name="Acesse Também" ></ItemMenu>
-                        <header className="App-header">
-                            <BoxTitle>
-                                <h3>SMARTCLASS</h3>
-                            </BoxTitle>
-                        </header>
-                        <ItemMenu name="Aulas de Graduação e Pós-Graduação" ></ItemMenu>
-                        <header className="App-header">
-                            <BoxTitle>
-                                <h3>AVA</h3>
-                            </BoxTitle>
-                        </header>
-                        <ItemMenu name="Orchestra4edu" ></ItemMenu>
-                        <header className="App-header">
-                            <BoxTitle>
-                                <h3>CURSOS LIVRES</h3>
-                            </BoxTitle>
-                        </header>
-                        <ItemMenu name="Acessar" ></ItemMenu>
-                        <ItemMenu name="Cupom" ></ItemMenu>
-                        <header className="App-header">
-                            <BoxTitle>
-                                <h3>PROVA FÁCIL</h3>
-                            </BoxTitle>
-                        </header>
-                        <ItemMenu name="Acessar" ></ItemMenu>
-                        <div className="arrow-right"></div>
-                        <List>
-                            {/* <ul
+  function handleNotasFaltasClick() {
+    setShowNotasFaltas(true); // Define o estado para mostrar a tela de Notas e Faltas
+  }
+
+  // Renderiza a tela de Notas e Faltas se o estado showNotasEFaltas for verdadeiro
+  // if (showNotasFaltas) {
+  //     return <NotasFaltas />;
+  // }
+  // Renderiza o componente Menu
+  return (
+    <StyledDiv>
+      <BoxSmall>
+        <div
+          className="menu-vertical_all menu-expand"
+          id="menu-vertical_secretaria_online"
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          <header className="App-header">
+            <BoxTitle>
+              <h3>ACESSO RÁPIDO</h3>
+            </BoxTitle>
+          </header>
+          <BoxText>
+            <ItemMenu name="Carteirinha de Estudante"></ItemMenu>
+            <ItemMenu name="Secretaria Online"></ItemMenu>
+            <ItemMenu name="Financeiro Online"></ItemMenu>
+            <ItemMenu name="Biblioteca"></ItemMenu>
+            <ItemMenu name="PAI"></ItemMenu>
+            <ItemMenu name="CPA"></ItemMenu>
+            <ItemMenu name="Atividade Complementares"></ItemMenu>
+            <ItemMenu name="Ouvidoria"></ItemMenu>
+            <ItemMenu
+              name="Requerimentos
+                            (Secretaria/Financeiro)"
+            ></ItemMenu>
+            <ItemMenu name="Contato dos Professores"></ItemMenu>
+            <ItemMenu name="Acesse Também"></ItemMenu>
+            <header className="App-header">
+              <BoxTitle>
+                <h3>SMARTCLASS</h3>
+              </BoxTitle>
+            </header>
+            <ItemMenu name="Aulas de Graduação e Pós-Graduação"></ItemMenu>
+            <header className="App-header">
+              <BoxTitle>
+                <h3>AVA</h3>
+              </BoxTitle>
+            </header>
+            <ItemMenu name="Orchestra4edu"></ItemMenu>
+            <header className="App-header">
+              <BoxTitle>
+                <h3>CURSOS LIVRES</h3>
+              </BoxTitle>
+            </header>
+            <ItemMenu name="Acessar"></ItemMenu>
+            <ItemMenu name="Cupom"></ItemMenu>
+            <header className="App-header">
+              <BoxTitle>
+                <h3>PROVA FÁCIL</h3>
+              </BoxTitle>
+            </header>
+            <ItemMenu name="Acessar"></ItemMenu>
+            <div className="arrow-right"></div>
+            <List>
+              {/* <ul
                             className="submenu-expand"
                             id="submenu_secretaria_online"
                             style={{ display: showSubMenu ? 'block' : 'none' }}> */}
-                            <li className="menu-vertical_all">
-                                <a href="../NotasFaltas/index.js" onClick={handleNotasFaltasClick}>
-                                    <i className="icon-plus"></i>Notas e Faltas
-                                </a>
-                            </li>
-                            <li className="menu-vertical_all">
-                                <a href="/aluno/horario-aula.php">
-                                    <i className="icon-plus"></i>Horários
-                                </a>
-                            </li>
-                            <li className="menu-vertical_all">
-                                <a href="/aluno/meus-documentos.php">
-                                    <i className="icon-plus"></i>Documentos
-                                </a>
-                            </li>
-                           {/*  </ul> */}
-                        </List>
-                        </BoxText>
-                    </div>
-                </BoxSmall>
-        </StyledDiv>
+              <li className="menu-vertical_all">
+                <a
+                  href="../NotasFaltas/index.js"
+                  onClick={handleNotasFaltasClick}
+                >
+                  <i className="icon-plus"></i>Notas e Faltas
+                </a>
+              </li>
+              <li className="menu-vertical_all">
+                <a href="/aluno/horario-aula.php">
+                  <i className="icon-plus"></i>Horários
+                </a>
+              </li>
+              <li className="menu-vertical_all">
+                <a href="/aluno/meus-documentos.php">
+                  <i className="icon-plus"></i>Documentos
+                </a>
+              </li>
+              {/*  </ul> */}
+            </List>
+          </BoxText>
+        </div>
+      </BoxSmall>
+    </StyledDiv>
   );
 }
 
